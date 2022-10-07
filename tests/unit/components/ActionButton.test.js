9876243f0a59e21e67fad98010d281782bab19d7
Mayflor <mayflor.tadena@smartstart.us>
2022-10-07 14:51:02 +0800

    add props on ActionButton component

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,27 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton";
+
+describe("ActionButton", () => {
+  it("renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "Im so clickable",
+        type: "primary",
+      },
+    });
+
+    expect(wrapper.text()).toMatch("Im so clickable");
+  });
+
+  it("applies one of several styles to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "Im so clickable",
+        type: "primary",
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  });
+});
